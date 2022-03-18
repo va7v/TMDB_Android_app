ef0c82386b666b3ae25aa0cfb5979f1c7b481b66
vadimvaleev <vadim.valeev@redone.app>
2022-03-18 18:55:07 +0500

    `Navigation`

--- app/src/main/java/r_studyproject/tmdbapp/RandomFragment.kt
+++ app/src/main/java/r_studyproject/tmdbapp/RandomFragment.kt
@@ -3,6 +3,7 @@ package r_studyproject.tmdbapp
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import r_studyproject.tmdbapp.databinding.FragmentRandomFilmBinding
 import r_studyproject.tmdbapp.databinding.FragmentRootBinding
 
@@ -14,7 +15,12 @@ class RandomFragment : Fragment(R.layout.fragment_random_film) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentRandomFilmBinding.bind(view)
 
-        // Details
+        binding.goBackButton.setOnClickListener {
+            // переходим по нижней кнопке Назад. Не закрывать
+            findNavController().popBackStack(R.id.rootFragment, false)
+            // навигация из туулбар. Действие не выходит из приложения
+            findNavController().navigateUp(R.id.action_randomFragment_to_detailsFragment)
+        }
 
     }
 }
